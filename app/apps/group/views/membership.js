211ef77beac1d8fb8177bd005397484f475e4a10
Ivo Petkov <ivo@ivopetkov.com>
2020-08-08 21:08:17 +0300

    Fix. The customize profile form is now only shown to the private users, when joining.

--- app/apps/group/views/membership.js
+++ app/apps/group/views/membership.js
@@ -24,21 +24,28 @@ async (args, library) => {
         container.add(x.makeButton('Join', async () => {
             var userID = x.currentUser.getID();
             var data = await x.services.call('profile', 'getData', { propertyType: 'user', propertyID: userID });
-            //if (await x.confirm('Are you sure you want to join this group?')) {
-            var result = await x.open('profile/form', {
-                groupUserID: groupID + '$' + userID,
-                image: data.image,
-                name: data.name,
-                description: data.description,
-                title: 'Customize your profile',
-                buttonText: 'Save and join group',
-                callServiceBefore: {
-                    appID: 'groups',
-                    name: 'join',
-                    args: { groupID: groupID }
+            if (x.currentUser.isPublic()) {
+                if (await x.confirm('Are you sure you want to join this group?')) {
+                    x.showLoading();
+                    await x.services.call('groups', 'join', { groupID: groupID });
+                    await x.back('joined');
                 }
-            }, { modal: true, width: 300 });
-            await x.back(result === 'saved' ? 'joined' : null);
+            } else {
+                var result = await x.open('profile/form', {
+                    groupUserID: groupID + '$' + userID,
+                    image: data.image,
+                    name: data.name,
+                    description: data.description,
+                    title: 'Customize your profile',
+                    buttonText: 'Save and join group',
+                    callServiceBefore: {
+                        appID: 'groups',
+                        name: 'join',
+                        args: { groupID: groupID }
+                    }
+                }, { modal: true, width: 300 });
+                await x.back(result === 'saved' ? 'joined' : null);
+            }
         }));
     }
     if (showLeaveButton) {
