f238b77d0870bf580a1ae820a72758a265443eb2
Ivo Petkov <ivo@ivopetkov.com>
2020-10-13 08:45:07 +0300

    Add links in the about window

--- app/apps/settings/views/about.js
+++ app/apps/settings/views/about.js
@@ -8,8 +8,8 @@ async (args, library) => {
     try {
         x.setTitle('About');
 
-        x.add(x.makeText('Dots Mesh is and open social platform. Visit about.dotsmesh.com for all the details.'));
-        x.add(x.makeText('The source code of this web app is available at github.com/dotsmesh'));
+        x.add(x.makeText('Dots Mesh is and open social platform. Visit <a onclick="x.openURL(\'https://about.dotsmesh.com/\');">about.dotsmesh.com</a> for all the details.', false, true));
+        x.add(x.makeText('The source code of this web app is available at <a onclick="x.openURL(\'https://github.com/dotsmesh/\');">github.com/dotsmesh</a>', false, true));
         x.add(x.makeText('App version: ' + x.version));
         x.add(x.makeButton('OK', async () => {
             await x.back();
