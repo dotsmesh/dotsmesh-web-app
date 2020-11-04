1a6ec327b292134e9daf76696b519f3a8c9d9244
Ivo Petkov <ivo@ivopetkov.com>
2020-11-05 00:02:17 +0200

    Make feedback field bigger and required

--- app/apps/settings/views/feedback.js
+++ app/apps/settings/views/feedback.js
@@ -10,12 +10,17 @@ async (args, library) => {
 
     x.add(x.makeText('Help make Dots Mesh even more awesome. Your ideas and suggestions are highly appreciated.'));
 
-    var fieldFeedback = x.makeFieldTextarea('', { placeholder: 'Your feedback' });
+    var fieldFeedback = x.makeFieldTextarea('', { placeholder: 'Your feedback', height: '200px' });
     x.add(fieldFeedback);
 
     x.add(x.makeHint('No additional or identifiable information will be sent.'));
 
     x.add(x.makeButton('Send', async () => {
+        var value = fieldFeedback.getValue().trim();
+        if (value.length === 0) {
+            x.alert('The feedback field is empty!');
+            return;
+        }
         x.showLoading();
         try {
             await fetch('https://about.dotsmesh.com/submitFeedback', {
@@ -23,7 +28,7 @@ async (args, library) => {
                 cache: 'no-cache',
                 headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
                 referrerPolicy: 'no-referrer',
-                body: JSON.stringify({ content: fieldFeedback.getValue() })
+                body: JSON.stringify({ content: value })
             });
         } catch (e) {
 
